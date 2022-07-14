964d4f58edf002b7b06a7e30c56d121bdb1ad41a
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-07-14 11:14:32 +0530

    [Madhavi] populate employee data

--- Employee-Payroll-JsonServer/js/EmployeePayroll.js
+++ Employee-Payroll-JsonServer/js/EmployeePayroll.js
@@ -24,3 +24,87 @@ salary.addEventListener('input', function(){
         }
     });
 }
+
+//getter and setter
+{
+    class EmployeePayroll
+    {
+        get id(){
+            return this._id;
+        }
+        set id(id){
+            this._id = id;
+        }
+    
+        get name() 
+        {
+            return this._name;
+        }
+        set name(name)
+        {
+            let nameCheck = RegExp('^[A-Z]{1,}[a-zA-Z]{2,}$')
+                if(nameCheck.test(name)){
+                this._name = name;
+            }
+            else{
+                throw "Name should start with capital letter and have atleast 3 letters";
+            }
+        }
+    
+        get profilePic(){
+            return this._profilePic;
+        }
+        set profilePic(profilePic){
+            this._profilePic = profilePic;
+        }
+    
+        get gender(){
+            return this._gender;
+        }
+        set gender(gender){
+            this._gender = gender;
+        }
+    
+        get department(){
+            return this._department;
+        }
+        set department(department){
+            this._department = department;
+        }
+    
+        get salary(){
+            return this._salary;
+        }
+        set salary(salary){
+            this._salary = salary;
+        }
+    
+        get startDate(){
+            return this._startDate;
+        }
+        set startDate(startDate){
+            let date = new Date().toLocaleDateString();
+            if(startDate <= date)
+            {
+                this._startDate = startDate;
+            }
+            else
+            {
+                throw "Invalid Date";
+            }
+        }
+    
+        get notes(){
+            return this._notes;
+        }
+        set notes(notes){
+            this._notes = notes;
+        }
+    
+        toString()
+        {
+            return "Id: " + this.id + ", Name: " + this.name + ", Profile Pic: " + this.profilePic + ", Gender: " + this.gender + 
+            ", Department: " + this.department + ", Salary: " + this.salary + ", StartDate: " + this.startDate + ", Notes: " + this.notes;
+        }
+    }
+}
