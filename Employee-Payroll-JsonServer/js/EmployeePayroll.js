f12c2df7b011c45777abba5a1700386996d556a5
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-07-14 10:52:54 +0530

    [Madhavi] add regular expression

--- Employee-Payroll-JsonServer/js/EmployeePayroll.js
+++ Employee-Payroll-JsonServer/js/EmployeePayroll.js
@@ -3,3 +3,24 @@ const message = document.querySelector('.salary-output');
 salary.addEventListener('input', function(){
     message.textContent = salary.value;
 });
+
+{
+    const name = document.querySelector('#name');
+    const message = document.querySelector('.nameError');
+    name.addEventListener('input', function()
+    {
+        let nameCheck = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
+        if(nameCheck.test(name.value))
+        {
+            message.textContent = "";
+        }
+        else if(name.value == "")
+        {
+            message.textContent = "";
+        }
+        else
+        {
+            message.textContent = "Wrong Name Format";
+        }
+    });
+}
